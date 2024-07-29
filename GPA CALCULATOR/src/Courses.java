ed04655d2dbfe3164e8ec94edc6abfd1c87356d9
Nuwe Bryant Nyero <nuwebryant@gmail.com>
2024-07-29 22:27:53 +0300

    course setter and getter methods

--- GPA CALCULATOR/src/Courses.java
+++ GPA CALCULATOR/src/Courses.java
@@ -6,4 +6,19 @@ public class Courses {
         this.CourseCode = CourseCode;
         this.CourseName = CourseName;
     }
+
+    public void setCourseCode(String CourseCode) {
+        this.CourseCode = CourseCode;
+    }
+
+    public void setCourseName(String CourseName) {
+        this.CourseName = CourseName;
+    }
+    
+    public String getCourseName() {
+        return CourseName;
+    }
+    public String getCourseCode() {
+        return CourseCode;
+    }
 }
